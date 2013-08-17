599c9179962079f0774fdc9711211fdeefb76df5
Pierre G. Richard <pgr@jaxo.com>
2013-08-17 07:25:03 +0200

    Step 4

--- source/chapter_2/js/pagebody.js
+++ source/chapter_2/js/pagebody.js
@@ -1,6 +1,18 @@
+window.addEventListener(
+   "load",
+   function() {
+      var elt = document.querySelector(
+         ".principal > div.pagebody[aria-expanded=true]"
+      );
+      if (elt) {
+         var toolbarId = elt.getAttribute("aria-owns");
+         if (toolbarId) showToolbar(toolbarId);
+      }
+   }
+);
 function revealPage(id) {
    var elt = document.getElementById(id);
-   var nodeName = elt.nodeName; // expect "DIV" (.body)
+   var nodeName = elt.nodeName; // expect "DIV" (.pagebody)
    var siblings = elt.parentNode.children;
    for (var i=0; i < siblings.length; ++i) {
       var sib = siblings[i];
--- source/chapter_2/js/toolbar.js
+++ source/chapter_2/js/toolbar.js
@@ -14,12 +14,10 @@ window.addEventListener(
 );
 function showToolbar(barId) {
    var menus = document.querySelectorAll("footer.toolbar > menu");
-   // var prevBarId = undefined;
    var container = menus[0].parentNode;
    container.display = "none";
    for (max=menus.length, i=0; i < max; ++i) {
       var menu = menus[i];
-      // if (menu.getAttribute("aria-expanded") == "true") prevBarId = menu.id;
       if (menu.id === barId) {
          container.style.display = "";
          menu.setAttribute("aria-expanded", "true");
@@ -27,5 +25,4 @@ function showToolbar(barId) {
          menu.setAttribute("aria-expanded", "false");
       }
    }
-   // return prevBarId;
 }
