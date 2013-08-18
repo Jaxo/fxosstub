c2fb44c5aee37b5e2301dcd2a08289112f01b653
Pierre G. Richard <pgr@jaxo.com>
2013-08-18 18:00:37 +0200

    Fixed nits for TOC

--- docs/toc.js
+++ docs/toc.js
@@ -1,6 +1,6 @@
 function buildToc() {
    var toc = "";
-   var elts = document.querySelectorAll("h1, h2");
+   var elts = document.querySelectorAll("h1, h2, h3");
    var lvl = 0;
    for (var i=0, max=elts.length; i < max; ++i) {
       var elt = elts[i];
@@ -13,20 +13,21 @@ function buildToc() {
       if (val > lvl) {
          do toc += "<ul>"; while (++lvl < val);
       }else if (val < lvl) {
-         do toc += "</ul>"; while (--lvl < val);
+         do toc += "</ul>"; while (--lvl > val);
       }
       toc += "<li><a href='#" +  id + "'>" + elt.textContent + "</a></li>";
    }
    while (lvl-- > 0) toc += "</ul>";
    toc += "<span id='toc_opener'>▼ </span>"
    var tocElt = document.getElementById("toc");
+   tocElt.style.height = "16px";
    tocElt.innerHTML = toc;
    document.getElementById("toc_opener").onclick = function() {
       var tocStyle = this.parentNode.style;
       if (this.getAttribute("aria-expanded")) {
          this.removeAttribute("aria-expanded");
          this.textContent = "▼ ";
-         tocStyle.height = "15px";
+         tocStyle.height = "16px";
          this.style.top = "-4px";
       }else {
          this.setAttribute("aria-expanded", "true");
